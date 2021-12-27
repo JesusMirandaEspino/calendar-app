532e5803b2ab16b827c4c9d8b04915b2d0a98a4b
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2021-12-27 10:05:35 -0600

    navbar

--- src/calendarApp.js
+++ src/calendarApp.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { AppRouter } from './router/AppRouter'
+import React from 'react';
+import { AppRouter } from './router/AppRouter';
+import { Navbar } from '../ui/Navbar';
 
 export const CalendarApp = () => {
     return (
         <div>
-            <AppRouter />
+            <Navbar />
         </div>
     )
 }
