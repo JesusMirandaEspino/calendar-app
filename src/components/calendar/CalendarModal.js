678f655e9eef4545431a8c5a3d6bde8147c1c584
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2021-12-30 12:02:08 -0600

    modal

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Modal from 'react-modal';
+
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
+
+    const [ isOpen, setIsOpen ] = useState(true)
+
+    const closeModal = () => {
+        setIsOpen(false);
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Modal 
+        className="modal"
+        overlayClassName="modal-fondo"
+        isOpen={ isOpen }
+        // onAfterOpen={afterOpenModal}
+        closeTimeoutMS={ 200 }
+        onRequestClose={closeModal}
+        style={customStyles}
+        >
+            <h1>Hola Mundo</h1>
+            <hr/>
+            <p>Hola de Nuevo...</p>
+        </Modal>
     )
 }
