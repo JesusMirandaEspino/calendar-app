b8c1d570d4ed508f844a720ebd11d18250ad1839
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-02-14 21:29:06 -0600

    authReducer

---
+++ src/reducers/authreducer.js
@@ -0,0 +1,21 @@
+
+
+const initialState = {
+    checking: true,
+    // uid: null,
+    // name: null
+}
+
+
+export const authReducer = ( state = initialState, action ) => {
+
+    switch ( action.type ) {
+
+        // code
+
+
+        default: return state;
+
+    }
+
+}
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { authReducer } from './authreducer';
 import { calendarReducer } from './calendarReducer';
 import { uiReducer } from './uiReducer';
 
 export const rootReducer =combineReducers({
     iu: uiReducer,
-    calendar: calendarReducer
+    calendar: calendarReducer,
+    auth: authReducer
 });
