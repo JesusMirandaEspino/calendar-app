441f58867fbc7a3630e175294dac65adc18706db
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-02-21 21:04:38 -0600

    router cheking

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 import {
@@ -8,7 +8,19 @@ import {
     Route
 } from "react-router-dom";
 
+import { useDispatch } from 'react-redux';
+import { startCheking } from '../actions/auth';
+
 export const AppRouter = () => {
+
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch( startCheking );
+    }, [])
+    
+
     return (
     <BrowserRouter>
             <Routes>
